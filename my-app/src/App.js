56b53483c66279b9e91d61bb4a255cd5a0fb29dd
DESKTOP-97E2TFH\Egzamin <przemekburnat50@gmail.com>
2024-09-30 10:53:52 +0200

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
+import react, {useState} from 'react';
 import './App.css';
 
 function App() {
+  // stany przechowujące dane formularza
+  const [tytuł, setTytuł] = useState("");
+  const [rodzaj,setRodzaj] = useState("")
+  // funkcja obsługująca przycisk "dodaj"
+ const handleSubmit = (e) => {
+  e.preventdefault(); //zapobiega odświeżaniu strony po kliknięciu
+
+  //wyświetlanie danych formularza w konsoli
+    console.log(`Tytuł: ${tytuł},rodzaj: ${rodzaj}`);
+  };
+
   return (
     <div className="App">
-      <form>
-        <div class="mb-3">
-          <label for="film" class="form-label">tytuł filmu</label>
-        <input type="text" class="form-cotrol" id="film" />
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      <div class="mb-3">
+        <label for="film" class="form-label">tytuł filmu</label>
+        <input type="text" class="form-control" id="film" />
+      </div>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>wybierz menu</option>
+        <option value="1">komedia</option>
+        <option value="2">obyczajowy</option>
+        <option value="3">sensacyjny</option>
+      </select>
     </div>
   );
 }
