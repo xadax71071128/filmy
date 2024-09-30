fbe9d723e06995551a3c10f09a1e37a9a750e018
DESKTOP-97E2TFH\Egzamin <przemekburnat50@gmail.com>
2024-09-30 11:24:44 +0200

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import react, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   // stany przechowujące dane formularza
@@ -7,7 +8,7 @@ function App() {
   const [rodzaj,setRodzaj] = useState("")
   // funkcja obsługująca przycisk "dodaj"
  const handleSubmit = (e) => {
-  e.preventdefault(); //zapobiega odświeżaniu strony po kliknięciu
+  e.preventDefault(); //zapobiega odświeżaniu strony po kliknięciu
 
   //wyświetlanie danych formularza w konsoli
     console.log(`Tytuł: ${tytuł},rodzaj: ${rodzaj}`);
@@ -16,16 +17,16 @@ function App() {
   return (
     <div className="App">
       <div class="mb-3">
-        <label for="film" class="form-label">tytuł filmu</label>
-        <input type="text" class="form-control" id="film" />
+        <label for="film" className="form-label">tytuł filmu</label>
+        <input type="text" className="form-control" id="film" />
       </div>
-      <select class="form-select" aria-label="Default select example">
+      <select className="form-select" aria-label="Default select example">
         <option selected>wybierz menu</option>
         <option value="1">komedia</option>
         <option value="2">obyczajowy</option>
         <option value="3">sensacyjny</option>
       </select>
-     <div><button type="submit" class="btn btn-primary">submit</button></div>
+     <div><button type="submit" class="btn btn-primary" onClick={handleSubmit}>submit</button></div>
     </div>
   );
 }
